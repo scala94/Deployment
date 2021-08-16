62877e5d54a4ea61b2e1f4ec3f47351486690ed2
unknown <simscalabrin@outlook.it>
2021-08-16 18:23:17 +0200

    first commit

--- src/components/Info.js
+++ src/components/Info.js
@@ -13,14 +13,14 @@ var experiences = [
         title: 'European Central Bank, Frankfurt am Main, Germany',
         role: 'IT Application Development Specialist – DGIS/DAS',
         date: 'March 2021 - Present',
-        description: ["Worked in Tableau Smart Automation processes’ with Python on an Administrative level (through Tableau Rest API).",
-                        "Involved with data centralization (ECB Big Data support), responsible for the redesign the Survey of Professional Forecasters process and its migration from Oracle to Hive/Impala based system (DISC/SPACE)"]
+        description: ["Worked for Tableau Smart Automation processes with Python on an Administrative level (through Tableau Rest API).",
+                        "Involved with data centralization (ECB Big Data support), responsible for the redesign the Survey of Professional Forecasters process and data migration from Oracle to Hive/Impala based system (DISC/SPACE)."]
     },
     {
         title: 'European Central Bank, Frankfurt am Main, Germany',
         role: "Trainee Analyst in DG Economics – Prices and Costs (DGE-PAC)",
         date: 'February 2020 - February 2021',
-        description: ["supported the Forecast Team producing statistical analysis, visualisation solutions and preparing presentation of results for the ECB Macroeconomic Projection Exercises.", 
+        description: ["Supported the Forecast Team producing statistical analysis, visualisation solutions and preparing presentation of results for the ECB (Broad) Macroeconomic Projection Exercises.", 
                         "Worked on different Smart Automation processes in R (databases update, ECB official chartpack library).", 
                         "Provided web scraping support with Python (through Scrapy)."]
     },
@@ -29,7 +29,7 @@ var experiences = [
         role: "Graduate Consultant, Teradata GDC",
         date: 'September 2019 - January 2020',
         description: ["Trained in the Teradata Data Warehouse technology, in data collection, production and dissemination processes. Learned fundamental concepts of database Design / Architecture and Administration.", 
-                        "Developed and applied back-end skills in projects and shadowing Lufthansa Airline client (advance SQL and Shell scripting).", 
+                        "Developed and applied back-end skills in projects and shadowing Lufthansa Airline client (mainly SQL and Shell scripting).", 
                         "Introduced and worked with Big Data Hadoop based Systems (through HIVE++ and Apache Sparks).", 
                         "Adopted principles of the Agile framework."]
     }, {
@@ -46,21 +46,21 @@ var education = [
         school: 'niversidade Catolica Portuguesa, Lisbon, Portugal',
         date: 'September 2017 - October 2019',
         description: ['Quantitative program focused on Macroeconomics and Microeconomics modelling, Econometrics, Macro Policies.',
-                        'Published thesis: “Flat-tax and minimum income experiment in an OLG model of Italy”. The purpose was to simulate and quantify the impact of a pending fiscal policy in Italy over different cohort ages inan Overlapping Generation Model (with Python).']
+                        'Published thesis: “Flat-tax and minimum income experiment in an OLG model of Italy” which aims to simulate and quantify the impact of a pending fiscal policy in Italy over different cohort ages in an Overlapping Generation Model (with Python).']
     },
     {
         title: 'Exchange program: Finance and Proramming',
         school: 'VSE University of Economics, Prague, Czech Republic',
         date: 'September 2018 - January 2019',
-        description: ['Intense Intermediate programming courses with R', 
-                        'Gained knowledge over financial matter (Advanced Derivatives, Business Valuation, Stochastic Processes)',
-                        'Developed interest for machine learning with Credit Risk modelling and management.']
+        description: ['Intense Intermediate programming courses with R.', 
+                        'Gained knowledge over financial matter (Advanced Derivatives, Business Valuation, Stochastic Processes).',
+                        'Developed interest for machine learning with Credit Risk Modelling and Management. I had the opportunity to apply my skills in projects for the evaluation of the Default Probability in Non Performing Loans (NPL).']
     },
     {
         title: 'Bachelor Degree: Economics and Business',
         school: 'Universitá Ca’ Foscari, Venice, Italy',
         date: 'September 2013 - July 2017',
-        description: ['Thesis on Blockchain technology: full insight over the Bitcoin Blockchain and showcase of potential applications with Smart Contracts (Ethereum project).']
+        description: ['Bachelor thesis on Blockchain technology: full insight over the Bitcoin Blockchain and showcase of potential applications with Smart Contracts (Ethereum project).']
     }
 ]
 
@@ -75,7 +75,7 @@ export default class Info extends Component {
                         backgoundSize: "cover",
                     }}>
                     <h1>About me</h1>
-                    <h2 style={{ textAlign: 'justify' }}> Hello visitor, my name is Giacomo Scalabrin and I am working IT Application Development Specialist for the European Central Bank. With a background in Economics, a good predisposition toward data analysis and a strong passion for programming, I built up through time an interesting and diversified set of skills.</h2>
+                    <h2 style={{ textAlign: 'justify' }}> Hello visitor, my name is Giacomo Scalabrin and I am an IT Application Development Specialist at the European Central Bank. With a background in Economics, a good predisposition toward data analysis and a strong passion for programming, I built up through time an interesting and diversified set of skills.</h2>
                     <div className="info-grid">
                         <Grid Item xs={12}>
                             <Grid container className='resume_timeline'>
--- src/components/Services.js
+++ src/components/Services.js
@@ -17,24 +17,27 @@ const Services = () => {
             img="https://th.bing.com/th/id/R.e2f00aab38a8792f5a66440db7419eea?rik=yBwIzn6iztcE%2fw&pid=ImgRaw&r=0"
             title="Economics"
             description="Having completed my studies in Quantitative Economics, I gained experience working as an Analyst for the European Central Bank (Economics department, Prices and Costs division)."
-            edescription=""
-
+            edescription="With a good predisposition for data analysis and substantial knowledge over the Economic theory (focused on Macro and Micro modelling), my profile fits Economist / Economic Analyst roles. Check my Master Thesis in the Portfolio section."
             />
             <Card
             img="https://blog.qintesi.com/hubfs/data-analytics-pianificazione-finanziaria.jpg"
             title="Data Analytics"
-            description="Strongly oriented toward statistical matter, I developed interest and gained experience with data analysis and most spread Machine Learning algorithms."  
+            description="Strongly oriented toward statistical matter, I developed interest and gained experience with data analysis and the most spread Machine Learning algorithms."  
+            edescription="Overall understanding over most common ML techniques, I was able to apply my skills in different University and personal projects. Worked with common unsupervised methods such as clustering, separatrix, classification trees and random forests and supervised like Logistics, OLS, GLS regressions. Recently expanding my horizons with online courses on Neural Network and Deep Network techniques."
              />
             <Card
             img="https://th.bing.com/th/id/OIP.RTMwlEQN_vjs6kzlge_0EQHaDE?pid=ImgDet&rs=1"
             title="IT & Smart Automation"
             description="Trained as back-end developer consultant at Teradata data Warehouse and currently working as IT Application Development Specialist at the European Central Bank's IT department."
+            edescription="Experienced in working in the IT environment, through time I started to adopt a good mindset for Smart Automation. The satisfaction that comes from reducing manual workload by automating mechanical routine tasks have no price to me. Whether it is about automating data collection, production, dissemination processes, or automating Administrative processes for some platform / software through API, I believe any environment have its own routines, leading to margins for automation."
                />
             <Card
             img="https://www.ishir.com/wp-content/uploads/2018/03/new-2.jpg"
             title="Software Development"
             description="Developed a strong passion for programming, I am expanding my technical skill-set through time in different directions."
+            edescription="Started as self-taught progammer during bachelor years, I broaded my technical skills with programming courses at VSE University and working in the IT environment. My passion for coding involves different fields, and I could apply what I learnt in a moltitude of personal projects (Web, Game, App Development). Check my portfolio for some example."
             />
+        <div className="serv-bottom"><h1></h1></div>
         </div>
         </div>
     )
@@ -53,7 +56,7 @@ function Card(props) {
                 <Collapse in={open}>
                     <div id="collapse"  className="expand-description">
                         <h1 style={{ textAlign: 'justify' }}>{props.edescription}</h1>
-                        <a href="/portfolio" className="btn-expand">check my portfolio</a>
+                        <a href="/portfolio" className="btn-expand">PORTFOLIO</a>
                     </div>
                 </Collapse>
             </div>
